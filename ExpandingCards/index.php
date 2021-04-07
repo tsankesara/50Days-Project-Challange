1780110c7acf265addc278350ea514f6490a2416
tsankesara <teerthsankesara@gmail.com>
2021-04-07 11:08:57 +0530

    Expanding Cards Done

---
+++ ExpandingCards/index.php
@@ -0,0 +1,7 @@
+<?php 
+define('PROJECT_ROOT_PATH', __DIR__);
+
+// echo PROJECT_ROOT_PATH . '/'
+
+include_once(PROJECT_ROOT_PATH . '/home.html'); 
+?>
---
+++ README.md
@@ -0,0 +1,2 @@
+# 50Days-Project-Challange
+I will be creating a projects with plain HTML, CSS and JavaScript every day for 50 days.
